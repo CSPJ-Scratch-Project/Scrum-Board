a4fad370d300e64022ad3b9e114acd9e3c18b673
Jonah <jonahpageweinbaum@gmail.com>
2023-04-22 15:26:41 -0700

    add route handlers for adding and getting tasks

--- server/server.js
+++ server/server.js
@@ -63,6 +63,50 @@ app.get('/projects', (req, res) => {
   });
 });
 
+// Get all projects across users
+app.get('/projects/all', (req, res) => {
+  pool.query('SELECT * FROM projects', (error, results) => {
+    if (error) {
+      throw error;
+    }
+    res.status(200).json(results.rows);
+  });
+});
+
+// Add new task
+// Req body should include project_id and task name
+app.post('/tasks', (req, res) => {
+  const { project_id, name } = req.body;
+  pool.query('INSERT INTO tasks (project_id, task_name) VALUES ($1, $2)', [project_id, name], (error, results) => {
+    if (error) {
+      console.log('couldnt add task');
+      throw error;
+    }
+    res.status(201).send(`Task added.`);
+  });
+});
+
+// Get all tasks for a project
+app.get('/tasks', (req, res) => {
+  const { project_id } = req.body;
+  pool.query('SELECT * FROM tasks WHERE project_id = $1', [project_id], (error, results) => {
+    if (error) {
+      throw error;
+    }
+    res.status(200).json(results.rows);
+  });
+});
+
+// Get all tasks across projects
+app.get('/tasks/all', (req, res) => {
+  pool.query('SELECT * FROM tasks', (error, results) => {
+    if (error) {
+      throw error;
+    }
+    res.status(200).json(results.rows);
+  });
+});
+
 
 app.listen(3000, () => {
   console.log(`Server listening on port 3000`);
