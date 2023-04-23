56a5264d716cf6c3f90818cef4c3db6dfb889c1b
PetrosO123 <pmo20@miami.edu>
2023-04-23 14:27:58 -0400

    added user profile form, visual container, user, altered button apdisplay

--- client/components/ProjectContainer.jsx
+++ client/components/ProjectContainer.jsx
@@ -21,11 +21,32 @@ export const ProjectContainer = () => {
         );
     };
 
-    return (<div>
-        HelloHelloHello
+    return (<div className = "outer" style = {{border: '1px solid black', width: '455px', display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
+        <div className="topBarContainer" style = {{display: 'flex', border: '3px solid yellow', width: '100%'}}>
+            {/* <div className="topLeftContainer">        
+            </div> */}
+            <div className = "topLeftContainer" style = {{width: '100%', height: '100%', border: '3px solid red', display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
+                <div id="image-container" style ={{
+                    width: '50%',
+                    height: '100px',
+                    backgroundSize: 'cover',
+                    backgroundPosition: 'center',
+                    border: '3px solid black',
+                    marginTop: '10px'
+                    }}>
+                </div>
+                <form style={{textAlign: 'center'}}>
+                    <input type="file" id="image-upload" style={{display: "block", margin: '10px 0px 10px 0px', marginLeft: '60px'}}/>
+                </form>
+            </div>
+            <div style = {{width: '100%', border: '3px solid green', display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
+                User 1
+            </div>
+        </div>
+        
         <div className="projectContainer" style = {{overflowY: 'auto', height: '700px', width: '450px', border: '3px solid black', display: 'flex', alignItems: 'center', flexDirection: 'column'}}>
             {arrContainer}
         </div>
-        <button onClick={handleAddProject}>Add Project</button>
+        <button  style = {{width: '100px', height: '50px'}} onClick={handleAddProject}>add new +</button>
     </div>)
 }
