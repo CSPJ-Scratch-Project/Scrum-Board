d7032d636c3ded417f06ad74308a1f8ec8b94f36
Scott Kim <skottcim@gmail.com>
2023-04-23 18:25:52 -0700

    functional double-click to edit text fields on tasks

--- client/components/Task.jsx
+++ client/components/Task.jsx
@@ -1,11 +1,60 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Task.css'
 
 const Task = ({ taskInfo, key }) => {
+  // hooks for title text edit field
+  const [ toggleTitle, setToggleTitle ] = useState(true);
+  const [ taskTitle, setTaskTitle] = useState('Title')
+  // hooks for body text edit field
+  const [ toggleBody, setToggleBody ] = useState(true);
+  const [ taskBody, setTaskBody] = useState('Body')
+
+
   return (
     <article className='taskBox'>
-      <h3 className='taskTitle'>task title</h3>
-      <p className='taskBody'>task body</p>
+      
+      {/* click to edit field for task title */}
+      {toggleTitle 
+        ? (
+        <p onDoubleClick={() => {setToggleTitle(false)}} >{taskTitle}</p> ) 
+        : (
+        <input
+          type="text"
+          value={taskTitle}
+          onChange={(event) => {
+            setTaskTitle(event.target.value)
+          }}
+          onKeyDown={(event) => {
+            if (event.key === 'Enter' || event.key === 'Escape') {
+              setToggleTitle(true)
+              event.preventDefault()
+              event.stopPropagation()
+            }
+          }}
+        />
+      )}
+
+      {/* click to edit field for task body */}
+      {toggleBody 
+        ? (
+        <p onDoubleClick={() => {setToggleBody(false)}} >{taskBody}</p> ) 
+        : (
+        <input
+          type="text"
+          value={taskBody}
+          onChange={(event) => {
+            setTaskBody(event.target.value)
+          }}
+          onKeyDown={(event) => {
+            if (event.key === 'Enter' || event.key === 'Escape') {
+              setToggleBody(true)
+              event.preventDefault()
+              event.stopPropagation()
+            }
+          }}
+        />
+      )}
+
     </article>
   )
 }
